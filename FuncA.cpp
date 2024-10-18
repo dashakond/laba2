0bb8c32b5ed4995a120659e337c14079a5a4f0cb
daryna <dasha.kondratez@gmail.com>
2024-10-18 17:37:17 +0300

    Hotfix: update FuncA to return sum of first 3 elements of sequence

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,5 +1,10 @@
+// FuncA.cpp
 #include "FuncA.h"
 
 double FuncA::calculate() {
-    return 0.0;
+    double sum = 0.0;
+    for (int i = 1; i <= 3; ++i) {
+        sum += 1.0 / i;
+    }
+    return sum;
 }
